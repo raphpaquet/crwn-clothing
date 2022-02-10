f1f0c6308c3c0169f76c95dc664bfb08dcc061b7
Raphaelle Paquet <68092222+raphpaquet@users.noreply.github.com>
2022-02-10 08:44:30 +0100

    created signin page with sign in with google

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { auth } from "../../firebase/firebase.utils";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.scss";
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link to="/" className="logo-container">
       <Logo className="logo"></Logo>
@@ -18,9 +19,16 @@ const Header = () => (
       <Link to="/contact" className="option">
         CONTACT
       </Link>
-      <Link to="/signin" className="option">
-        SIGN IN
-      </Link>
+
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
     </div>
   </div>
 );
