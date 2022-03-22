5d5e8f3005296603840f948b820b6759235e3bb7
Raphaelle Paquet <68092222+raphpaquet@users.noreply.github.com>
2022-03-22 16:29:28 -0700

    rename pages to route

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
+import HomePage from "./routes/homepage/homepage.component";
+import ShopPage from "./routes/shop/shop.component";
 import Header from "./components/header/header.component";
-import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import SignInAndSignUpPage from "./routes/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
